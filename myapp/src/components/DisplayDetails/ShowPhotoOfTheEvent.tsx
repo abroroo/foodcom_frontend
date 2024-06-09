1145efac3c466c571706bdadf5d21e109afcccb9
Adam <abroriskandarov99@gmail.com>
2024-06-09 17:11:17 +0900

    bugfix: other event picture will not show

--- myapp/src/components/DisplayDetails/ShowPhotoOfTheEvent.tsx
+++ myapp/src/components/DisplayDetails/ShowPhotoOfTheEvent.tsx
@@ -11,37 +11,37 @@ export const ShowPhotoOfTheEvent = ({
   imageFirstParent,
   isMouseWithinHero,
 }: ShowPhotoOfTheEventProps) => {
-  const { event, themeColor } = useGlobalState()
-  const selectedEvent = event
+  const { event: selectedEvent, themeColor } = useGlobalState()
   const selectedEventConfig = EventsConfig.find(
     (event) => event.value === selectedEvent
   )
-  const imageSrc = selectedEventConfig
-    ? selectedEventConfig.imageSrc
-    : "images/real/festival.jpg"
+  const imageSrc = selectedEventConfig ? selectedEventConfig.imageSrc : ""
 
   return (
-    <motion.div
-      initial={{ opacity: 0, x: 120 }}
-      whileInView={{ opacity: 1, x: 0 }}
-      ref={imageFirstParent}
-      transition={{ duration: 0.5, type: "spring", bounce: 0.3 }}
-      className={selectedEvent ? "flex cursor-pointer flex-col" : "hidden"}
-    >
-      <p className="mb-3 flex w-[98%] justify-start text-sm">
-        {" "}
-        행사사진 구경하기
-      </p>
-      <motion.img
-        alt="행사사진"
-        src={imageSrc}
-        style={{ width: 450, height: 450 }}
-        className={`mb-[5px] mr-[5px] rounded brightness-110 ${
-          isMouseWithinHero
-            ? " scale-[102%] duration-700"
-            : "scale-100 duration-700"
-        }`}
-      />
-    </motion.div>
+    <>
+      {imageSrc !== "" && (
+        <motion.div
+          initial={{ opacity: 0, x: 120 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          ref={imageFirstParent}
+          transition={{ duration: 0.5, type: "spring", bounce: 0.3 }}
+          className={selectedEvent ? "flex cursor-pointer flex-col" : "hidden"}
+        >
+          <p className="mb-3 flex w-[98%] justify-start text-sm">
+            행사사진 구경하기
+          </p>
+          <motion.img
+            alt="행사사진"
+            src={imageSrc}
+            style={{ width: 450, height: 450 }}
+            className={`mb-[5px] mr-[5px] rounded brightness-110 ${
+              isMouseWithinHero
+                ? "scale-[102%] duration-700"
+                : "scale-100 duration-700"
+            }`}
+          />
+        </motion.div>
+      )}
+    </>
   )
 }
